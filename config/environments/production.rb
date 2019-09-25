b9fbd1ee4fa745957bc05162e06da9f38a6ec994
Mike <ruecktmi@gmail.com>
2019-09-25 14:41:22 -0700

    fix automated confirmation email link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {:host => 'ruck-photo-app.herokuappcom', :protocal => 'https'}
+  config.action_mailer.default_url_options = {:host => 'ruck-photo-app.herokuapp.com', :protocal => 'https'}
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
